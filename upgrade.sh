6d414b49c4318c4d31af44c5518bc7a7fc89ae37
doctorsaid <connect2fb@gmail.com>
2016-08-28 11:37:06 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,12 +2,12 @@
 
 sudo sh -c 'echo "y\n\n" | DEBIAN_FRONTEND=noninteractive do-release-upgrade -d -f DistUpgradeViewNonInteractive' &
 
-while :
-do
-	echo "Press [CTRL+C] to stop.."
-  free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
-  df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
-  top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
-	sleep 5
-done
+# while :
+# do
+# 	echo "Press [CTRL+C] to stop.."
+#   free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+#   df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
+#   top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
+# 	sleep 5
+# done
 
